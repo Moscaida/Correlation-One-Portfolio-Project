7e3c29d1818d913115073350319b78f95f2893b3
Javier Pastor <95084901+Moscaida@users.noreply.github.com>
2023-07-07 14:24:29 -0700

    darkmode

---
+++ MyWebsite/Javascript/darkmode.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body');
+const modeToggle = document.getElementById('mode-toggle');
+const modeStatus = document.querySelector('.mode-status');
+function toggleMode() {
+body.classList.toggle('dark-mode');
+
+const modeMessage = body.classList.contains('dark-mode') ?
+    'Dark Mode'
+    : "Light Mode"
+
+modeStatus.innerText = "Currently in " + modeMessage;
+  }
+
+modeToggle.addEventListener('click', toggleMode);
