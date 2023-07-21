4797acef9d3f88fe52ca86092b1539577aa7ce04
Javier Pastor <95084901+Moscaida@users.noreply.github.com>
2023-07-20 22:02:41 -0700

    On branch master Changes to be committed: commit	modified:   MyWebsite/3d-prints.html commit	deleted:    MyWebsite/Javascript/darkmode.js commit	new file:   MyWebsite/Javascript/script.js commit	modified:   MyWebsite/css/mywebsite.css commit  modified:   MyWebsite/index.html

---
+++ MyWebsite/Javascript/script.js
@@ -0,0 +1,35 @@
+//Dark Mode//
+const body = document.querySelector('body');
+const modeToggle = document.getElementById('mode-toggle');
+const modeStatus = document.querySelector('.mode-status');
+function toggleMode() {
+body.classList.toggle('dark-mode');
+
+const modeMessage = body.classList.contains('dark-mode') ?
+    'Dark Mode'
+    : "Light Mode"
+
+modeStatus.innerText = "Currently in " + modeMessage;
+  }
+
+modeToggle.addEventListener('click', toggleMode);
+
+//Fade and Slide//
+
+const items = document.querySelectorAll('.card')
+
+const options = {
+  threshold: 0.5
+}
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('slide-in');
+    }
+  });
+}, options)
+
+items.forEach(item => {
+  observer.observe(item);
+})
